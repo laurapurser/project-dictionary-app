4800cd16c69c73acc206db239eb22a2176c7f14d
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-07-12 10:10:55 +0100

    Added phonetic text and audio

--- src/Results.js
+++ src/Results.js
@@ -2,11 +2,26 @@ import React from "react";
 import Meaning from "./Meaning.js";
 
 export default function Results(props) {
-  //console.log(props.results);
+  console.log(props.results.phonetics);
   if (props.results) {
     return (
       <div className="Results">
         <h3>{props.results.word}</h3>
+        {props.results.phonetics.map(function (phonetic, index) {
+          if (phonetic.audio) {
+            return (
+              <div key={index}>
+                <a href={phonetic.audio} target="_blank">
+                  Listen
+                </a>{" "}
+                {phonetic.text}{" "}
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+
         {props.results.meanings.map(function (meanings, index) {
           return (
             <div key={index}>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,21 +2,18 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  console.log(props.synonyms);
   if (props.synonyms) {
     return (
       <div className="Synonyms">
         {props.synonyms.map(function (synonyms, index) {
           return (
-            <div>
-              <ul className="synonym-list">
-                <li key={index}>
-                  <em>
-                    <strong> {synonyms} </strong>
-                  </em>
-                </li>
-              </ul>
-            </div>
+            <ul className="synonym-list">
+              <li key={index}>
+                <em>
+                  <strong> {synonyms} </strong>
+                </em>
+              </li>
+            </ul>
           );
         })}
       </div>
