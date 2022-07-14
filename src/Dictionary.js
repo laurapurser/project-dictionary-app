e6d537949febdca55fba509f7d0ecc00d53d78c1
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-07-14 10:16:09 +0100

    Adjusted app size and added title

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,58 +6,54 @@ import Results from "./Results";
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState("");
-  let [loaded, setLoaded]= useState(false);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     console.log(response.data);
     setResults(response.data[0]);
   }
-  function search(){
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
   //[0].meanings[0].definitions[0].definition
   function handleSearch(event) {
     event.preventDefault();
-search();
-    
+    search();
   }
 
   function handleKeywordChange(event) {
     event.preventDefault();
     setKeyword(event.target.value);
   }
-  function load(){
+  function load() {
     setLoaded(true);
     search();
   }
 
-if (loaded) {
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h3> What word would you like to look up?</h3>
+          <form onSubmit={handleSearch}>
+            <input
+              type="search"
+              placeholder="Enter a word"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="form-text">
+            Suggested words: sunset, yoga, plant...
+          </div>
+        </section>
 
-
-  return (
-    
-    <div className="Dictionary">
-    <section>
-      <form onSubmit={handleSearch}>
-        <input
-          type="search"
-          placeholder="Enter a word"
-          onChange={handleKeywordChange}
-        />
-        
-      </form>
-      <div className="form-text">
-      Suggested words: sunset, yoga, plant...
+        <Results results={results} />
       </div>
-      </section>
-     
-      <Results results={results} />
-    </div>
-  );
-} else {
-   
-  load() 
-  return "Loading";
-}
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
