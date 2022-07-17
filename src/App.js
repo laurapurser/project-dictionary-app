84344b18e6708d9ac0c18670db0332f3dc8bef2a
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-07-17 20:19:01 +0100

    Changed info in footer

--- src/App.js
+++ src/App.js
@@ -6,9 +6,28 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">Online Dictionary</header>
-        <Dictionary defaultKeyword="sunset"/>
+        <Dictionary defaultKeyword="sunset" />
 
-        <footer className="App-footer">Coded by Laura Purser</footer>
+        <footer className="App-footer">
+          Coded by{" "}
+          <a
+            href="https://objective-einstein-d849c5.netlify.app"
+            target="_blank"
+          >
+            Laura Purser
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/laurapurser/project-dictionary-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a href="https://timely-cuchufli-86fe4b.netlify.app/" target="_blank">
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
